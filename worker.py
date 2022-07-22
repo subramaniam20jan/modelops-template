5299c87f2ec6339aa9c71820d2d6e0801d0ced6d
Subramaniam Ramasubramanian <subramaniam.r20jan@gmail.com>
2022-07-22 07:23:16 +0000

    Fix functinoality

--- worker.py
+++ worker.py
@@ -3,7 +3,11 @@ import typer
 app = typer.Typer()
 
 @app.command("echo")
-def cmd_echo(
-    text: str = typer.Option("test", help="What to echo?")
-):
-    print(f"Hello {text}")
+def echo_cmd(text: str = typer.Option("test", help="What to echo?")):
+    """
+    Echo what you pass!
+    """
+    typer.echo(f"Hello {text}")
+
+if __name__ == "__main__":
+    app()
